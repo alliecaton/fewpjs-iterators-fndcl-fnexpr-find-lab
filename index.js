c235b1b06f2395f076e76825248c0fdc4aaa926d
Allie <alliecaton3@gmail.com>
2021-03-26 12:52:06 -0400

    commit tree 567ccf86875b600fd0b05d827bf6afcdecdbf255

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
+
+function superbowlWin(array) {
+    let winner = array.find(obj => obj.result === "W" )
+    if (winner) {
+        return winner.year
+    }
+}
